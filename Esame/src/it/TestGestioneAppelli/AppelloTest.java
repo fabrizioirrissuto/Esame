31ff2651d123da367a785eb3ce5bd34288f69b30
fabrizio <fabrizio@192.168.1.6>
2019-12-18 12:07:09 +0100

    Commit progetto pronto

---
+++ Esame/src/it/TestGestioneAppelli/AppelloTest.java
@@ -0,0 +1,110 @@
+package it.TestGestioneAppelli;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import control.GestioneAppello;
+import control.GestioneCorso;
+import entity.Appello;
+import entity.Corso;
+import entity.Data;
+import entity.Docente;
+import entity.Studente;
+
+public class AppelloTest {
+
+	
+
+
+	@Test
+	public void test1VisualizzaStudentiPrenotati() {
+		GestioneAppello gestioneappello=new GestioneAppello();
+		boolean lista_vuota;
+		lista_vuota=false;
+		int id_appello;
+		id_appello=7;
+		lista_vuota=gestioneappello.NessunAppello();
+		assertTrue(lista_vuota);
+		
+		
+	}
+	@Test
+	public void test2VisualizzaStudentiPrenotati() {
+		GestioneAppello gestioneappello=new GestioneAppello();
+		int id_appello;
+		id_appello=7;
+		Appello appelloAppoggio=null;
+		appelloAppoggio=gestioneappello.TrovaAppello(id_appello);
+		assertNull(appelloAppoggio);
+		
+		
+	}
+	@Test
+	public void test3VisualizzaStudentiPrenotati() {
+		boolean nessuno;
+		nessuno=false;
+		int id_app;
+		id_app=7;
+		GestioneAppello gestioneappello=new GestioneAppello();
+		GestioneCorso gestionecorso=new GestioneCorso();
+		 Docente Docente1=new Docente(52,"Roberto","Natella");
+		Corso corso=new Corso(140,"ingegneria del software",9);
+		LocalDate  data_evento;
+		data_evento = LocalDate.of(2019, Month.AUGUST, 31);
+		Data data=new Data(data_evento,"scritta","sgt5");
+		corso.Associa_docente(Docente1);
+		Appello appellob=new Appello(7);
+		appellob.associaCorso(corso);
+		appellob.AssociaData(data);
+		gestioneappello.Aggiungi_Appello(appellob);
+		Appello appelloapp=null;
+		appelloapp=gestioneappello.TrovaAppello(id_app);
+		nessuno=appelloapp.NessunoStudentePrenotato();
+		assertTrue(nessuno);
+		
+		
+		
+	}
+	@Test
+	public void test4VisualizzaStudentiPrenotati() {
+		boolean presenti;
+		presenti=false;
+		int id_app;
+		id_app=7;
+		GestioneAppello gestioneappello=new GestioneAppello();
+		GestioneCorso gestionecorso=new GestioneCorso();
+		 Docente Docente1=new Docente(52,"Roberto","Natella");
+		Corso corso=new Corso(140,"ingegneria del software",9);
+		LocalDate  data_evento;
+		data_evento = LocalDate.of(2019, Month.AUGUST, 31);
+		Data data=new Data(data_evento,"scritta","sgt5");
+		corso.Associa_docente(Docente1);
+		Appello appellob=new Appello(7);
+		appellob.associaCorso(corso);
+		appellob.AssociaData(data);
+		gestioneappello.Aggiungi_Appello(appellob);
+		Appello appelloapp=null;
+		appelloapp=gestioneappello.TrovaAppello(id_app);
+		Studente studente=new Studente(44,"Fabrizio","Irrissuto");
+		appellob.InserisciStudentePrenotato(studente);
+		presenti=appellob.NessunoStudentePrenotato();
+		assertFalse(presenti);
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+
+}
